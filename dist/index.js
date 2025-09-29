91d43371100d9ef672dbd39fce8932f66a8835fd
howardsolutions <phungthinh0946519356@gmail.com>
2025-09-29 11:45:31 +0700

    feat: servers

--- dist/index.js
+++ dist/index.js
@@ -1,7 +1,12 @@
 import express from "express";
 const app = express();
 const PORT = 8080;
-app.use(express.static("."));
+app.use("/app", express.static("./src/app"));
+app.get("/healthz", handlerReadiness);
+function handlerReadiness(req, res) {
+    res.set("Content-Type", "text/plain");
+    res.send("OK");
+}
 app.listen(PORT, () => {
     console.log(`Server is running at http://localhost:${PORT}`);
 });
--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,14 @@ import express from "express";
 const app = express();
 const PORT = 8080;
 
-app.use(express.static("."));
+app.use("/app", express.static("./src/app"));
+
+app.get("/healthz", handlerReadiness);
+
+function handlerReadiness(req: express.Request, res: express.Response) {
+  res.set("Content-Type", "text/plain; charset=utf-8");
+  res.send("OK");
+}
 
 app.listen(PORT, () => {
   console.log(`Server is running at http://localhost:${PORT}`);
