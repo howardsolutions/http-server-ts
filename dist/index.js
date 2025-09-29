6cc32211286cf9096315c865172162635fffe769
howardsolutions <phungthinh0946519356@gmail.com>
2025-09-29 14:58:23 +0700

    feat: middleware

--- dist/index.js
+++ dist/index.js
@@ -1,12 +1,26 @@
 import express from "express";
 const app = express();
 const PORT = 8080;
+// Static middleware to serve files and images
 app.use("/app", express.static("./src/app"));
+// log response middleware
+app.use(middlewareLogResponses);
 app.get("/healthz", handlerReadiness);
 function handlerReadiness(req, res) {
-    res.set("Content-Type", "text/plain");
+    res.set("Content-Type", "text/plain; charset=utf-8");
     res.send("OK");
 }
+// middleware fns
+function middlewareLogResponses(req, res, next) {
+    res.on("finish", () => {
+        const statusCode = res.statusCode;
+        if (statusCode !== 200) {
+            console.log(`[NON-OK] ${req.method} ${req.url} - Status: ${statusCode}`);
+        }
+    });
+    next();
+}
+;
 app.listen(PORT, () => {
     console.log(`Server is running at http://localhost:${PORT}`);
 });
--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,11 @@ import express from "express";
 const app = express();
 const PORT = 8080;
 
+// Static middleware to serve files and images
 app.use("/app", express.static("./src/app"));
+
+// log response middleware
+app.use(middlewareLogResponses)
 
 app.get("/healthz", handlerReadiness);
 
@@ -11,6 +15,19 @@ function handlerReadiness(req: express.Request, res: express.Response) {
   res.set("Content-Type", "text/plain; charset=utf-8");
   res.send("OK");
 }
+
+// middleware fns
+
+function middlewareLogResponses(req: express.Request, res: express.Response, next: express.NextFunction) {
+  res.on("finish", () => {
+    const statusCode = res.statusCode;
+    if (statusCode !== 200) {
+      console.log(`[NON-OK] ${req.method} ${req.url} - Status: ${statusCode}`);
+    }
+  });
+
+  next()
+};
 
 app.listen(PORT, () => {
   console.log(`Server is running at http://localhost:${PORT}`);
